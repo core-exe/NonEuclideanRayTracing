6748ae0a18fc88c72dcb405e3f0f97c8683b803b
core_exe <2873912972@qq.com>
2021-11-18 23:40:34 +0800

    Initial commit

---
+++ build/CMakeFiles/PA1.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/class90_2019012387/workspace/ComputerGraphics/homework1/src/image.cpp" "/home/class90_2019012387/workspace/ComputerGraphics/homework1/build/CMakeFiles/PA1.dir/src/image.cpp.o"
+  "/home/class90_2019012387/workspace/ComputerGraphics/homework1/src/main.cpp" "/home/class90_2019012387/workspace/ComputerGraphics/homework1/build/CMakeFiles/PA1.dir/src/main.cpp.o"
+  "/home/class90_2019012387/workspace/ComputerGraphics/homework1/src/mesh.cpp" "/home/class90_2019012387/workspace/ComputerGraphics/homework1/build/CMakeFiles/PA1.dir/src/mesh.cpp.o"
+  "/home/class90_2019012387/workspace/ComputerGraphics/homework1/src/scene_parser.cpp" "/home/class90_2019012387/workspace/ComputerGraphics/homework1/build/CMakeFiles/PA1.dir/src/scene_parser.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../deps/vecmath/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/class90_2019012387/workspace/ComputerGraphics/homework1/build/deps/vecmath/CMakeFiles/vecmath.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
